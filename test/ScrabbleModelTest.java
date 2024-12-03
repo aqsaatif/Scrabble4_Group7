09ab61420b6c1e2e4f8eddb2b78a565ac094edc9
yomnaibrahim <yomnaibrahim@cmail.carleton.ca>
2024-12-02 19:58:05 -0500

    updated JavaDocs for tests

--- test/ScrabbleModelTest.java
+++ test/ScrabbleModelTest.java
@@ -1,172 +1,244 @@
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.Assert;
+/**
+ * The modelTest class
+ * @author Lujain Jdue
+ * @version 1, November 10, 2024
+ *
+ * @author Yomna Ibrahim
+ * @version 2, November 24, 2024
+ *
+ */
+
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class ScrabbleModelTest {
     private ScrabbleModel model;
     private Player player1;
     private Player player2;
     private Player player3;
-    private List<Tile> tiles;
-    private List<Tile> tiles2;
-    private List<Tile> tiles3;
+    private List<Tile> tiles, tiles2, tiles3;
     private String word1 = "task";
     private String word2 = "kits";
+
     private char[][] board;
+
     int row = 1;
     int col = 1;
     int col2 = 4;
+
     private int[][] premiumSquares;
+
     private ScrabbleAI ai;
 
-    public ScrabbleModelTest() {
-    }
-
+    /**
+     * Setup common variables and objects to be used among different test cases
+     */
     @Before
-    public void setUp() {
-        this.model = new ScrabbleModel(2, 1);
-        this.player1 = new Player("Player 1");
-        this.player2 = new Player("Player 2");
-        this.player3 = new Player("AI Player 1");
-        this.player3.setIsAI(true);
-        this.ai = new ScrabbleAI(this.model, new Dictionary());
-        this.premiumSquares = new int[15][15];
-        this.model.initializePremiumSquares();
-        this.tiles = new ArrayList();
-        this.tiles.add(new Tile("T", 1));
-        this.tiles.add(new Tile("A", 1));
-        this.tiles.add(new Tile("S", 1));
-        this.tiles.add(new Tile("K", 5));
-        this.tiles.add(new Tile("E", 1));
-        this.tiles.add(new Tile("F", 4));
-        this.tiles.add(new Tile("G", 2));
-        this.tiles2 = new ArrayList();
-        this.tiles2.add(new Tile("T", 1));
-        this.tiles2.add(new Tile("I", 1));
-        this.tiles2.add(new Tile("S", 1));
-        this.tiles2.add(new Tile("K", 5));
-        this.tiles2.add(new Tile("E", 1));
-        this.tiles2.add(new Tile("F", 4));
-        this.tiles2.add(new Tile("G", 2));
-        this.tiles3 = new ArrayList();
-        this.tiles3.add(new Tile("H", 4));
-        this.tiles3.add(new Tile("A", 1));
-        this.tiles3.add(new Tile("V", 4));
-        this.tiles3.add(new Tile("E", 1));
-        this.tiles3.add(new Tile("L", 1));
-        this.tiles3.add(new Tile("E", 1));
-        this.tiles3.add(new Tile("T", 1));
+    public void setUp(){
+        model = new ScrabbleModel(2,1);
+        player1 = new Player("Player 1");
+        player2 = new Player("Player 2");
+        player3 = new Player("AI Player 1");
+        player3.setIsAI(true);
+
+        ai = new ScrabbleAI(model, new Dictionary());
+
+        premiumSquares = new int[ScrabbleModel.SIZE][ScrabbleModel.SIZE];
+        model.initializePremiumSquares();
+
+        tiles = new ArrayList<>();
+        tiles.add(new Tile("T", 1));
+        tiles.add(new Tile("A", 1));
+        tiles.add(new Tile("S", 1));
+        tiles.add(new Tile("K", 5));
+        tiles.add(new Tile("E", 1));
+        tiles.add(new Tile("F", 4));
+        tiles.add(new Tile("G", 2));
+
+        tiles2 = new ArrayList<>();
+        tiles2.add(new Tile("T", 1));
+        tiles2.add(new Tile("I", 1));
+        tiles2.add(new Tile("S", 1));
+        tiles2.add(new Tile("K", 5));
+        tiles2.add(new Tile("E", 1));
+        tiles2.add(new Tile("F", 4));
+        tiles2.add(new Tile("G", 2));
+
+        tiles3 = new ArrayList<>();
+        tiles3.add(new Tile("H", 4));
+        tiles3.add(new Tile("A", 1));
+        tiles3.add(new Tile("V", 4));
+        tiles3.add(new Tile("E", 1));
+        tiles3.add(new Tile("L", 1));
+        tiles3.add(new Tile("E", 1));
+        tiles3.add(new Tile("T", 1));
     }
 
+    /**
+     * test that AI players are AI and normal players are normal
+     */
     @Test
-    public void testAINormal() {
-        Assert.assertFalse(this.player1.isAI());
-        Assert.assertFalse(this.player2.isAI());
-        Assert.assertTrue(this.player3.isAI());
+    public void testAINormal(){
+        assertFalse(player1.isAI());
+        assertFalse(player2.isAI());
+        assertTrue(player3.isAI());
+
     }
 
+    /**
+     * test that AI playing on empty board
+     */
     @Test
-    public void testAIPlay() {
+    public void testAIPlay(){
         ScrabbleModelViewFrame.NUMOFPLAYERS = 2;
-        this.player3.addTiles(this.tiles3);
-        this.player1.addTiles(this.tiles);
-        this.model.changeTurn();
-        Assert.assertTrue(this.model.checkEmptyBoard());
-        ScrabbleAI var10000 = this.ai;
-        ScrabbleAI.playBestMove(this.player3);
-        Assert.assertFalse(this.model.checkEmptyBoard());
+        player3.addTiles(tiles3);
+        player1.addTiles(tiles);
+        model.changeTurn();
+
+        assertTrue(model.checkEmptyBoard());
+
+        ai.playBestMove(player3);
+
+        assertFalse(model.checkEmptyBoard()); // board no longer empty, AI was able to place a word
+
     }
 
-    @Test
-    public void testInitialEmptyBoard() {
-        char[][] board = this.model.getBoard();
 
-        for(int i = 0; i < 15; ++i) {
-            for(int j = 0; j < 15; ++j) {
-                Assert.assertEquals(32L, (long)board[i][j]);
+    /**
+     * test that the initial setup of the board is empty and ready to play on
+     */
+    @Test
+    public void testInitialEmptyBoard(){
+        char[][] board = model.getBoard();
+
+        //check that every cell is initially empty
+        for(int i = 0; i < ScrabbleModel.SIZE; i++){
+            for (int j =0; j < ScrabbleModel.SIZE; j++){
+                assertEquals(' ', board[i][j]);
             }
         }
 
     }
-
+    /**
+     * test that the word can be placed horizontally successfully
+     */
     @Test
-    public void testPlaceWordHorizontalSuccess() {
-        Player currentPlayer = this.model.getCurrentPlayer();
-        currentPlayer.addTiles(this.tiles);
-        String[] result = this.model.placeWord(this.word1, this.row, this.col, true, currentPlayer);
-        this.model.currentPlayer.calculatePoints(this.word1, this.row, this.col, true, this.premiumSquares);
-        Assert.assertEquals("true", result[0]);
-        Assert.assertEquals((long)this.word1.charAt(0), (long)this.model.getBoard()[this.row - 1][this.col - 1]);
-        Assert.assertEquals((long)this.word1.charAt(1), (long)this.model.getBoard()[this.row - 1][this.col]);
-        Assert.assertEquals((long)this.word1.charAt(2), (long)this.model.getBoard()[this.row - 1][this.col + 1]);
-        Assert.assertEquals((long)this.word1.charAt(3), (long)this.model.getBoard()[this.row - 1][this.col + 2]);
-        Assert.assertEquals(8L, (long)currentPlayer.getScore());
+    public void testPlaceWordHorizontalSuccess(){
+        Player currentPlayer = model.getCurrentPlayer();
+        //mock that the player has the necessary letters in hand
+        currentPlayer.addTiles(tiles);
+        String[] result = model.placeWord(word1, row, col, true, currentPlayer);
+        model.currentPlayer.calculatePoints(word1,row,col, true, premiumSquares);
+
+        assertEquals("true", result[0]);
+        assertEquals(word1.charAt(0), model.getBoard()[row-1][col-1]);
+        assertEquals(word1.charAt(1), model.getBoard()[row-1][col]);
+        assertEquals(word1.charAt(2), model.getBoard()[row-1][col+1]);
+        assertEquals(word1.charAt(3), model.getBoard()[row-1][col+2]);
+
+        assertEquals(8, currentPlayer.getScore());
+
     }
 
+    /**
+     * test that teh word can be placed vertically successfully
+     */
     @Test
-    public void testPlaceWordVerticalSuccess() {
-        Player currentPlayer = this.model.getCurrentPlayer();
-        currentPlayer.addTiles(this.tiles2);
-        String[] result = this.model.placeWord(this.word2, this.row, 4, false, currentPlayer);
-        this.model.currentPlayer.calculatePoints(this.word2, this.row, 4, false, this.premiumSquares);
-        Assert.assertEquals("true", result[0]);
-        Assert.assertEquals((long)this.word2.charAt(0), (long)this.model.getBoard()[this.row - 1][this.col2 - 1]);
-        Assert.assertEquals((long)this.word2.charAt(1), (long)this.model.getBoard()[this.row][this.col2 - 1]);
-        Assert.assertEquals((long)this.word2.charAt(2), (long)this.model.getBoard()[this.row + 1][this.col2 - 1]);
-        Assert.assertEquals((long)this.word2.charAt(3), (long)this.model.getBoard()[this.row + 2][this.col2 - 1]);
-        Assert.assertEquals(8L, (long)currentPlayer.getScore());
+    public void testPlaceWordVerticalSuccess(){
+        Player currentPlayer = model.getCurrentPlayer();
+        currentPlayer.addTiles(tiles2);
+        String [] result = model.placeWord(word2, row, 4, false, currentPlayer);
+        model.currentPlayer.calculatePoints(word2, row, 4, false, premiumSquares);
+
+        assertEquals("true", result[0]);
+        assertEquals(word2.charAt(0), model.getBoard()[row-1][col2-1]);
+        assertEquals(word2.charAt(1), model.getBoard()[row][col2-1]);
+        assertEquals(word2.charAt(2), model.getBoard()[row+1][col2-1]);
+        assertEquals(word2.charAt(3), model.getBoard()[row+2][col2-1]);
+
+        assertEquals(8, currentPlayer.getScore());
     }
 
+    /**
+     * test an invalid word input to check that it will not be placed on board
+     */
     @Test
-    public void testFailedWordPlacement() {
-        String word2 = "toolongword";
+    public void testFailedWordPlacement(){
+        String word2 = "toolongword"; //not in dictionary
         int row2 = 1;
         int col2 = 13;
-        this.player1.addTiles(this.tiles);
-        String[] result = this.model.placeWord(word2, row2, col2, true, this.player1);
-        Assert.assertEquals("false", result[0]);
+
+        player1.addTiles(tiles);
+        String[] result = model.placeWord(word2, row2, col2, true, player1);
+
+        assertEquals("false", result[0]);
+
     }
 
+    /**
+     * test that player can only place word within bounds of SIZE = 15
+     */
     @Test
-    public void testFailedWordPlacementOutOfBounds() {
+    public void testFailedWordPlacementOutOfBounds(){
         int row2 = 20;
         int col2 = 22;
-        this.player1.addTiles(this.tiles);
-        String[] result = this.model.placeWord(this.word2, row2, col2, true, this.player1);
-        Assert.assertEquals("false", result[0]);
+
+        player1.addTiles(tiles);
+        String[] result = model.placeWord(word2, row2, col2, true, player1);
+
+        assertEquals("false", result[0]);
+
     }
 
+    /**
+     * test that player cannot place word if the player does not have the tiles for that word
+     */
     @Test
-    public void testFailedWordPlacementNoTiles() {
+    public void testFailedWordPlacementNoTiles(){
         String word = "zone";
         int row2 = 1;
         int col2 = 1;
-        this.player1.addTiles(this.tiles);
-        String[] result = this.model.placeWord(word, row2, col2, true, this.player1);
-        Assert.assertEquals("false", result[0]);
+
+        player1.addTiles(tiles);
+        String[] result = model.placeWord(word, row2, col2, true, player1);
+
+        assertEquals("false", result[0]);
+
     }
+    /**
+     * test the placement of player2 word that it fails if it is not connected to player1 word
+     */
 
     @Test
-    public void testPlaceWordPlayer2Fail() {
-        Player currentPlayer = this.model.getCurrentPlayer();
-        currentPlayer.addTiles(this.tiles2);
-        String[] result = this.model.placeWord(this.word2, this.row, 4, false, currentPlayer);
-        this.model.currentPlayer.calculatePoints(this.word2, this.row, 4, false, this.premiumSquares);
-        Assert.assertEquals("true", result[0]);
-        Assert.assertEquals(8L, (long)currentPlayer.getScore());
-        currentPlayer = this.player2;
-        this.player2.addTiles(this.tiles);
-        String[] result2 = this.model.placeWord(this.word1, 1, 10, true, this.player2);
-        Assert.assertEquals("false", result2[0]);
-        Assert.assertEquals(0L, (long)currentPlayer.getScore());
+    public void testPlaceWordPlayer2Fail(){
+        Player currentPlayer = model.getCurrentPlayer();
+        currentPlayer.addTiles(tiles2);
+        String [] result = model.placeWord(word2, row, 4, false, currentPlayer);
+        model.currentPlayer.calculatePoints(word2, row, 4, false, premiumSquares);
+
+        assertEquals("true", result[0]);
+        assertEquals(8, currentPlayer.getScore());
+
+        currentPlayer = player2;
+        player2.addTiles(tiles);
+        String [] result2 = model.placeWord(word1, 1, 10, true, player2);
+
+        assertEquals("false", result2[0]);
+        assertEquals(0, currentPlayer.getScore());
     }
 
+    /**
+     * test that the tiles used are removed and replaced with new tiles
+     */
     @Test
-    public void testTileRemoval() {
-        this.player1.addTiles(this.tiles);
-        this.model.placeWord(this.word1, 7, 7, true, this.player1);
-        Assert.assertEquals(7L, (long)this.player1.getTiles().size());
+    public void testTileRemoval(){
+        player1.addTiles(tiles);
+        model.placeWord(word1, 7, 7, true, player1);
+        assertEquals(7, player1.getTiles().size());
     }
 }
